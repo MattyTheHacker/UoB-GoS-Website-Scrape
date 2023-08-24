d6ea2c4c10e1ab801bcc678bea24f175c894cc0c
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2023-08-24 23:57:58 +0100

    add git pull

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,9 @@
 # cd to correct directory
 cd /home/pi/Documents/UoB-GoS-Website-Scrape/src/
 
+# pull any changes from github
+git pull
+
 # Run the python script
 python scrape_without_prompts.py
 
