5a76d73c90cb9e4a73f8b6e8f54e70048179c71e
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2025-02-13 11:14:50 +0000

    add using venv

--- src/run.sh
+++ src/run.sh
@@ -6,11 +6,17 @@ cd /home/cogs/Documents/UoB-GoS-Website-Scrape/src/
 # pull any changes from github
 git pull -X theirs
 
+# Use the venv
+source venv/bin/activate
+
 # Run the python script
 python scrape_without_prompts.py
 
 # Run the events script
 python event_utilts.py
+
+# Run the emails script
+python strip_emails.py
 
 # Add all files to git
 git add --all
